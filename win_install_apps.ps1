89d9085238a95b403db1975683021916b3d25ea6
SZone <107401110+realSZone@users.noreply.github.com>
2026-07-21 13:31:29 +0200

    Create win_install_apps.ps1

---
+++ win_install_apps.ps1
@@ -0,0 +1,14 @@
+$packages = @(
+    "DisplayLink.GraphicsDriver"
+    "Google.Chrome"
+    "SlackTechnologies.Slack"
+    "Google.GoogleDrive"
+    "AgileBits.1Password"
+    "7zip.7zip"
+)
+
+foreach ($package in $packages) {
+    winget install --id $package -e `
+        --accept-package-agreements `
+        --accept-source-agreements
+}
