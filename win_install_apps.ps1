6d2e4ac3bb9a7c93f65567e4bf3e0923278b0fc2
SZone <107401110+realSZone@users.noreply.github.com>
2026-07-21 13:42:46 +0200

    Update win_install_apps.ps1

--- win_install_apps.ps1
+++ win_install_apps.ps1
@@ -1,14 +1,1 @@
-$packages = @(
-    "DisplayLink.GraphicsDriver"
-    "Google.Chrome"
-    "SlackTechnologies.Slack"
-    "Google.GoogleDrive"
-    "AgileBits.1Password"
-    "7zip.7zip"
-)
-
-foreach ($package in $packages) {
-    winget install --id $package -e `
-        --accept-package-agreements `
-        --accept-source-agreements
-}
+$packages = @( "DisplayLink.GraphicsDriver" "Google.Chrome" "SlackTechnologies.Slack" "Google.GoogleDrive" "AgileBits.1Password" "7zip.7zip" ) foreach ($package in $packages) { winget install ` --id $package ` --exact ` --source winget ` --accept-package-agreements ` --accept-source-agreements }
